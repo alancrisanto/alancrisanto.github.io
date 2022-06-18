37824162fa0cc39a3dac4eeb5c7cc4e6679f6704
acrisanto <alanvcrisanto@gmail.com>
2022-06-17 22:55:09 -0500

    chore: footer and main added

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -1,5 +1,26 @@
+const currentDate = new Date();
+
 const hamButton = document.querySelector(".ham");
 const navigation = document.querySelector(".navigation");
+const year = document.querySelector(".year");
+const lastUpdate = document.querySelector(".updated");
+
+year.textContent = currentDate.getFullYear();
+
+try {
+	let options = {
+		weekday: "long",
+		day: "numeric",
+		month: "short",
+		year: "numeric",
+	};
+
+	lastUpdate.textContent = `Last updated on ${currentDate.toDateString("en-us", options)} ${currentDate.toLocaleString(
+		"en-us",
+	)}`;
+} catch (error) {
+	alert("Error displaying time update");
+}
 
 const displayList = () => {
 	navigation.classList.toggle("responsive");
